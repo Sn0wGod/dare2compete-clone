53aed29d7a5bdfea5e9f6e3cd13cb79ec37aa99f
Shailesh Yogendra <shaileshyogendra1999@gmail.com>
2020-06-04 01:29:44 +0530

    Updates

---
+++ precache-manifest.981c2d40e2a09690658a5e0a850c8231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d80a11fed2be2cd22b7bcbbc9e208e",
+    "url": "/dare2compete-clone/index.html"
+  },
+  {
+    "revision": "838b53cd29ac3927d740",
+    "url": "/dare2compete-clone/static/css/main.038e216f.chunk.css"
+  },
+  {
+    "revision": "e8874cd0c70fcd79e3b6",
+    "url": "/dare2compete-clone/static/js/2.88abcad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dare2compete-clone/static/js/2.88abcad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838b53cd29ac3927d740",
+    "url": "/dare2compete-clone/static/js/main.ee44d1fa.chunk.js"
+  },
+  {
+    "revision": "0f8c2299deda2e162d75",
+    "url": "/dare2compete-clone/static/js/runtime-main.ca3281be.js"
+  }
+]);
